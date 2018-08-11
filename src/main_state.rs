c609874fee2f510e794b1c108067198d62e150fb
Kevin Wong <wong.kevin47@gmail.com>
2018-08-11 11:09:43 -0700

    Add Player movement

--- src/main_state.rs
+++ src/main_state.rs
@@ -1,4 +1,6 @@
 use ggez::*;
+use ggez::event::*;
+use ggez::timer::*;
 use ggez::graphics::{DrawMode, Point2};
 
 use player::Player;
@@ -7,6 +9,18 @@ use tile::Tile;
 
 const NUM_TILES_X : usize = 30;
 const NUM_TILES_Y : usize = 15;
+
+const PLAYER_SPEED : f32 = 2.0;
+
+const P1_UP : event::Keycode = Keycode::W;
+const P1_DOWN : event::Keycode = Keycode::S;
+const P1_LEFT : event::Keycode = Keycode::A;
+const P1_RIGHT : event::Keycode = Keycode::D;
+
+const P2_UP : event::Keycode = Keycode::Up;
+const P2_DOWN : event::Keycode = Keycode::Down;
+const P2_LEFT : event::Keycode = Keycode::Left;
+const P2_RIGHT : event::Keycode = Keycode::Right;
 
 pub struct MainState 
 {
@@ -33,6 +47,9 @@ impl event::EventHandler for MainState
 {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> 
     {
+        println!("FPS: {}", timer::get_fps(_ctx ) );
+        self.player1.update();
+        self.player2.update();
         Ok(())
     }
 
@@ -44,5 +61,37 @@ impl event::EventHandler for MainState
         self.player2.draw( ctx );
         graphics::present(ctx);
         Ok(())
+    }
+
+    fn key_down_event( &mut self, ctx: &mut Context, keycode: Keycode, _: Mod, _: bool )
+    {
+        match keycode
+        {
+            P1_UP => { self.player1.set_vel_y( -PLAYER_SPEED ) }
+            P1_DOWN => { self.player1.set_vel_y( PLAYER_SPEED ) }
+            P1_LEFT => { self.player1.set_vel_x( -PLAYER_SPEED ) }
+            P1_RIGHT => { self.player1.set_vel_x( PLAYER_SPEED ) }
+            P2_UP => { self.player2.set_vel_y( -PLAYER_SPEED ) }
+            P2_DOWN => { self.player2.set_vel_y( PLAYER_SPEED ) }
+            P2_LEFT => { self.player2.set_vel_x( -PLAYER_SPEED ) }
+            P2_RIGHT => { self.player2.set_vel_x( PLAYER_SPEED ) }
+            _ => {}
+        }
+    }
+
+    fn key_up_event( &mut self, ctx: &mut Context, keycode: Keycode, _: Mod, _: bool )
+    {
+        match keycode
+        {
+            P1_UP => { self.player1.set_vel_y( 0.0 ) }
+            P1_DOWN => { self.player1.set_vel_y( 0.0 ) }
+            P1_LEFT => { self.player1.set_vel_x( 0.0 ) }
+            P1_RIGHT => { self.player1.set_vel_x( 0.0 ) }
+            P2_UP => { self.player2.set_vel_y( 0.0 ) }
+            P2_DOWN => { self.player2.set_vel_y( 0.0 ) }
+            P2_LEFT => { self.player2.set_vel_x( 0.0 ) }
+            P2_RIGHT => { self.player2.set_vel_x( 0.0 ) }
+            _ => {}
+        }
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -5,10 +5,12 @@ use ggez::GameResult;
 pub struct Player
 {
     id: u32,
-    pos_x: u32,
-    pos_y: u32,
+    pos_x: f32,
+    pos_y: f32,
     width: u32,
     height: u32,
+    vel_x: f32,
+    vel_y: f32,
     sprite: graphics::Image,
 }
 
@@ -24,18 +26,36 @@ impl Player
         Player
         { 
             id, 
-            pos_x,
-            pos_y,
+            pos_x: pos_x as f32,
+            pos_y: pos_y as f32,
             width: 32,
             height: 32,
+            vel_x: 0.0,
+            vel_y: 0.0,
             sprite: graphics::Image::solid( ctx, 32, player_debug_color() ).unwrap(),
         }
     }
 
+    pub fn update( &mut self )
+    {
+        self.pos_x += self.vel_x;
+        self.pos_y += self.vel_y;
+    }
+
     pub fn draw( &mut self, ctx: &mut Context ) -> GameResult<()>
     {
-        let dest_point = graphics::Point2::new( self.pos_x as f32, self.pos_y as f32 );
+        let dest_point = graphics::Point2::new( self.pos_x, self.pos_y );
         graphics::draw(ctx, &self.sprite, dest_point, 0.0 );
         Ok(())
+    }
+
+    pub fn set_vel_x( &mut self, vel_x: f32 )
+    {
+        self.vel_x = vel_x;
+    }
+
+    pub fn set_vel_y( &mut self, vel_y: f32 )
+    {
+        self.vel_y = vel_y;
     }
 }
